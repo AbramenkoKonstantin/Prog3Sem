e6d47a97ed994a051425b92b41011800aafe0a39
Константин Абраменко <kostaabramenko695@gmail.com>
2021-10-10 20:31:24 +0700

    First

---
+++ ProgLab2/ProgLab2/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace ProgLab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxE = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.zedGraph = new ZedGraph.ZedGraphControl();
+            this.backButton = new System.Windows.Forms.Button();
+            this.forwardButton = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBoxF = new System.Windows.Forms.TextBox();
+            this.textBoxAnswer = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "a";
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(40, 91);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(100, 20);
+            this.textBoxA.TabIndex = 1;
+            // 
+            // textBoxE
+            // 
+            this.textBoxE.Location = new System.Drawing.Point(40, 168);
+            this.textBoxE.Name = "textBoxE";
+            this.textBoxE.Size = new System.Drawing.Size(100, 20);
+            this.textBoxE.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Точность";
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(40, 129);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(100, 20);
+            this.textBoxB.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(81, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "b";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(63, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Формула";
+            // 
+            // zedGraph
+            // 
+            this.zedGraph.Location = new System.Drawing.Point(189, 12);
+            this.zedGraph.Name = "zedGraph";
+            this.zedGraph.ScrollGrace = 0D;
+            this.zedGraph.ScrollMaxX = 0D;
+            this.zedGraph.ScrollMaxY = 0D;
+            this.zedGraph.ScrollMaxY2 = 0D;
+            this.zedGraph.ScrollMinX = 0D;
+            this.zedGraph.ScrollMinY = 0D;
+            this.zedGraph.ScrollMinY2 = 0D;
+            this.zedGraph.Size = new System.Drawing.Size(599, 331);
+            this.zedGraph.TabIndex = 6;
+            this.zedGraph.UseExtendedPrintDialog = true;
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(263, 349);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(196, 34);
+            this.backButton.TabIndex = 7;
+            this.backButton.Text = "Назад";
+            this.backButton.UseVisualStyleBackColor = true;
+            // 
+            // forwardButton
+            // 
+            this.forwardButton.Location = new System.Drawing.Point(515, 349);
+            this.forwardButton.Name = "forwardButton";
+            this.forwardButton.Size = new System.Drawing.Size(196, 34);
+            this.forwardButton.TabIndex = 8;
+            this.forwardButton.Text = "Вперед";
+            this.forwardButton.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.AutoSize = false;
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(31, 296);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(87, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "Рассчитать";
+            // 
+            // ToolStripMenuItem
+            // 
+            this.ToolStripMenuItem.Name = "ToolStripMenuItem";
+            this.ToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+            this.ToolStripMenuItem.Text = "Рассчитать";
+            this.ToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
+            // 
+            // textBoxF
+            // 
+            this.textBoxF.Location = new System.Drawing.Point(40, 210);
+            this.textBoxF.Name = "textBoxF";
+            this.textBoxF.Size = new System.Drawing.Size(100, 20);
+            this.textBoxF.TabIndex = 4;
+            // 
+            // textBoxAnswer
+            // 
+            this.textBoxAnswer.Enabled = false;
+            this.textBoxAnswer.Location = new System.Drawing.Point(40, 323);
+            this.textBoxAnswer.Name = "textBoxAnswer";
+            this.textBoxAnswer.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAnswer.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 395);
+            this.Controls.Add(this.textBoxAnswer);
+            this.Controls.Add(this.textBoxF);
+            this.Controls.Add(this.forwardButton);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.zedGraph);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxE);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxE;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private ZedGraph.ZedGraphControl zedGraph;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button forwardButton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBoxF;
+        private System.Windows.Forms.TextBox textBoxAnswer;
+    }
+}
+
---
+++ ProgLab2/ProgLab2/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+using MathNet.Symbolics;
+
+namespace ProgLab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                GraphPane pane = zedGraph.GraphPane;
+                pane.CurveList.Clear();
+                double.TryParse(textBoxA.Text, out double aBord);
+                double.TryParse(textBoxB.Text, out double bBord);
+                double.TryParse(textBoxE.Text, out double esp);
+                Expression func = Infix.ParseOrThrow(textBoxF.Text);
+
+                if (esp < 0)
+                {
+                    throw new Exception();
+                }
+
+                double espValue = esp;
+                int counter = 0;
+                while (espValue < 1)
+                {
+                    espValue *= 10;
+                    counter += 1;
+                }
+                PointPairList mPoint = new PointPairList();
+                List<double> xPoints = new List<double>();
+                List<double> yPoints = new List<double>();
+                int stepsCounter = -1;
+
+                LineDraw(aBord, bBord, esp, func);
+                double minPoint = 0;
+                while (Math.Abs(bBord - aBord) >= esp)
+                {
+                    double f = (1 + Math.Sqrt(5)) / 2;
+                    double x1 = bBord - ((bBord - aBord) / f);
+                    double x2 = aBord + ((bBord - aBord) / f);
+                    if (FuncValue(x1, func) >= FuncValue(x2, func))
+                    {
+                        aBord = x1;
+                    }
+                    else
+                    {
+                        bBord = x2;
+                    }
+                    minPoint = (aBord + bBord) / 2;
+                    stepsCounter += 1;
+                    xPoints.Add(minPoint);
+                    yPoints.Add(FuncValue(minPoint, func));
+                }
+                double point = Math.Round(FuncValue(minPoint, func), counter);
+                mPoint.Add(minPoint, point);
+                LineItem linePoint = pane.AddCurve("Point", mPoint, Color.Black, SymbolType.Default);
+                textBoxAnswer.Text = "(" + Math.Round(minPoint, counter).ToString() + ":" + Math.Round(FuncValue(minPoint, func), counter).ToString() + ")";
+            }
+            catch
+            {
+                if (textBoxA.Text == "" || textBoxB.Text == "" || textBoxE.Text == "" || textBoxF.Text == "")
+                {
+                    MessageBox.Show("Пустые поля недопустимы");
+                }
+                else if (double.Parse(textBoxE.Text) < 0)
+                {
+                    MessageBox.Show("Точность не может быть меньше 0");
+                }
+                else if (double.Parse(textBoxA.Text) >= double.Parse(textBoxB.Text))
+                {
+                    MessageBox.Show("Параметр a должен быть меньше параметра b");
+                }
+                else
+                {
+                    MessageBox.Show("Некорректно задана вычисляемая функция");
+                }
+            }
+        }
+        private void LineDraw(double aBord, double bBord, double esp, Expression func)
+        {
+            GraphPane pane = zedGraph.GraphPane;
+            pane.CurveList.Clear();
+
+            double espValue = esp;
+            int counter = 0;
+            while (espValue < 1)
+            {
+                espValue *= 10;
+                counter += 1;
+            }
+
+            if ((bBord - aBord) / 50000 > esp)
+            {
+                esp = (bBord - aBord) / 50000;
+            }
+
+            PointPairList list = new PointPairList();
+
+
+            for (double x = aBord; x <= bBord; x += esp)
+            {
+                double funcValue = Math.Round(FuncValue(x, func), counter);
+                list.Add(x, funcValue);
+            }
+
+            LineItem myCurve = pane.AddCurve("Sinc", list, Color.Blue, SymbolType.None);
+            zedGraph.AxisChange();
+            zedGraph.Invalidate();
+        }
+        private double FuncValue(double point, Expression func)
+        {
+            Dictionary<string, FloatingPoint> x = new Dictionary<string, FloatingPoint>()
+            {
+                { "x", point }
+            };
+            return Evaluate.Evaluate(x, func).RealValue;
+        }
+
+        private void Params_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            var textBox = (TextBox)sender;
+            if (char.IsDigit(e.KeyChar) || (e.KeyChar == ',' && textBox.Text.Contains(",") == false) || (e.KeyChar == '-' && textBox.Text == "") || (e.KeyChar == (char)Keys.Back))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
